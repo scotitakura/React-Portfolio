acdf73bc8fdb04241c38bb4e546e0177c101878f
sitakura <scot.itakura@gmail.com>
2020-10-07 10:00:30 -0700

    installed react app, set up file structures

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+
+
+
+export default App;
