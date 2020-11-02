5ed6522ed1d8fd664d691d2fefbb5daa08f8a1ad
sitakura <scot.itakura@gmail.com>
2020-11-02 12:48:24 -0800

    Updates

---
+++ precache-manifest.6e6aa809634a18b827fd9c5173710167.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c2d60967ba2a655348cfc14c417d84",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "daeebd8ca4c858f95edb",
+    "url": "/React-Portfolio/static/css/main.76544f99.chunk.css"
+  },
+  {
+    "revision": "37de648f052131f107b2",
+    "url": "/React-Portfolio/static/js/2.3dd3c299.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.3dd3c299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daeebd8ca4c858f95edb",
+    "url": "/React-Portfolio/static/js/main.be211ccc.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "42c6198d9dceec901ea5ed3af2204bad",
+    "url": "/React-Portfolio/static/media/CosmopolitianTastes-img.42c6198d.PNG"
+  },
+  {
+    "revision": "b535477d6c56b771248ea6d17639fa98",
+    "url": "/React-Portfolio/static/media/DSC02878.b535477d.JPG"
+  },
+  {
+    "revision": "7bd7af84b8aa6d621c3b6fee37e0fe96",
+    "url": "/React-Portfolio/static/media/QualitreesPreview.7bd7af84.PNG"
+  },
+  {
+    "revision": "f0eea7185d66b1647e6c0e5f15bd68c4",
+    "url": "/React-Portfolio/static/media/The-Craver.f0eea718.PNG"
+  }
+]);
